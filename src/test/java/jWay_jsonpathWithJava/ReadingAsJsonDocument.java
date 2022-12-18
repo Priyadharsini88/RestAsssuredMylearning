dcaf90f335a80aa061223909b085e54897d64293
babua <babua@LAPTOP-K6EVNUCN>
2022-12-18 18:57:53 +0530

    Parse json file one time

--- src/test/java/jWay_jsonpathWithJava/ReadingAsJsonDocument.java
+++ src/test/java/jWay_jsonpathWithJava/ReadingAsJsonDocument.java
@@ -1,9 +1,13 @@
 package jWay_jsonpathWithJava;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.InvalidJsonException;
 import com.jayway.jsonpath.JsonPath;
 
 public class ReadingAsJsonDocument {
@@ -21,11 +25,25 @@ public class ReadingAsJsonDocument {
 		
 		
 	}
+	
+	public static void ParseOneTime() throws InvalidJsonException, IOException
+	{
+		InputStream jsonFile= new FileInputStream("src/test/resources/bookstore.json");
+		
+		Object Parsedjsondoc=Configuration.defaultConfiguration().jsonProvider().parse(jsonFile.readAllBytes());
+		
+		List<Object> countries=JsonPath.read(Parsedjsondoc,"$..country");
+		
+		for(Object country:countries)
+		{
+			System.out.println(country);
+		}
+	}
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		readAJson();
-		
+		ParseOneTime();
 	}
 
 }
